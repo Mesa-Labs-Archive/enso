6178572f8a452ac6141a66aae0a6df5c6f989cc8
BlackMesa123 <brother12@hotmail.it>
2019-11-11 16:21:26 +0100

    enso: script: add enso_symlink_recursive & enso_fix_secontext
    +better vndk libs symlink & delay on video playback fix on dream2
    
    Signed-off-by: BlackMesa123 <brother12@hotmail.it>

--- script/bin/enso.sh
+++ script/bin/enso.sh
@@ -48,6 +48,15 @@ function enso_extractpkg() {
     fi;
 }
 
+function enso_fix_secontext() {
+    if [ ! -f "$1" ]; then
+        echo "E: enso_fix_secontext: $1 not found! Aborting..." >> "$LOGFILE";
+        enso_abort;
+    else
+        $BUSYBOX chcon -t "$1" "$2" >> "$LOGFILE";
+    fi;
+}
+
 function enso_initfields() {
     if [ "$(getprop ro.on.enso.debug)" = "true" ]; then
         LOGFILE="/system/on_enso.log";
@@ -70,6 +79,24 @@ function enso_symlink() {
     fi;
 }
 
+function enso_symlink_recursive() {
+    if [ ! -d "$1" ]; then
+        echo "E: enso_symlink_recursive: $1 not found, couldn't link to $2" >> "$LOGFILE";
+        enso_abort;
+    else
+        cd "$1" > "/dev/null";
+        for FILENAME in *; do
+            ORIGFILE="$1/$FILENAME";
+            LINKFILE="$2/$FILENAME";
+            if [ -f "$LINKFILE" ]; then
+                $BUSYBOX rm -f "$LINKFILE";
+            fi;
+            $BUSYBOX ln -s "$ORIGFILE" "$LINKFILE" >> "$LOGFILE";
+        done;
+        cd - > "/dev/null";
+    fi;
+}
+
 
 ## More life
 mount -o remount,rw /system;
@@ -79,7 +106,7 @@ enso_initfields;
 
 echo "" >> "$LOGFILE";
 echo "--- OnTheOne ensō first boot script ---" >> "$LOGFILE";
-echo "-- Init. /system at" $(date '+%D %T') "--" >> "$LOGFILE";
+echo "-- Init /system at" $(date '+%D %T') "--" >> "$LOGFILE";
 echo "" >> "$LOGFILE";
 
 # Check if device is compatible
@@ -89,10 +116,11 @@ enso_checksupport;
 echo "I: ensō: Applying device fixes" >> "$LOGFILE";
 if $IsDream2; then
     enso_extractpkg "/system/enso/dream2.onpkg" "/system";
-    enso_symlink "/vendor/lib/vndk/libaudioroute.so" "/system/lib/libaudioroute.so";
-    enso_symlink "/vendor/lib/vndk/libtinyalsa.so" "/system/lib/libtinyalsa.so";
-    enso_symlink "/vendor/lib64/vndk/libssl.so" "/system/lib64/libssl.so";
+    enso_fix_secontext "remotedisplay_exec" "/system/bin/remotedisplay";
 fi;
+
+enso_symlink_recursive "/vendor/lib/vndk" "/system/lib";
+enso_symlink_recursive "/vendor/lib64/vndk" "/system/lib64";
 
 if [ -f /system/enso/enso.prop ]; then
     echo "# ensō - device related props" >> /system/build.prop;
